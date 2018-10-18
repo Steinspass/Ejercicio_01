3d377be031ce0a020d78481b1f333540b108482d
Steinspass <37332713+Steinspass@users.noreply.github.com>
2018-10-19 01:26:59 +0200

    Cambios en MainActivity

--- app/src/main/java/com/example/ndpsh/ejercicio_01/MainActivity.java
+++ app/src/main/java/com/example/ndpsh/ejercicio_01/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Elementos UI
     private EditText EditText;
     private Button btnnext;
 
@@ -18,25 +20,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Forzar y cargar icono en el action bar
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_mylauncher);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
 
        EditText = (EditText) findViewById(R.id.editText);
        btnnext = (Button) findViewById(R.id.button);
 
-
-
        btnnext.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-               startActivity(intent);
+               String name = EditText.getText().toString();
+               if (name != null && !name.isEmpty()){
+                   Intent intent =new Intent(MainActivity.this, SecondActivity.class);
+                   intent.putExtra("name",name);
+                   startActivity(intent);
+               }else{
+                   Toast.makeText(MainActivity.this, "Name it is empty", Toast.LENGTH_LONG).show();
+               }
            }
        });
 
 
-    }
+           }
+       }
 
 
 
-}
+
+
+
+
